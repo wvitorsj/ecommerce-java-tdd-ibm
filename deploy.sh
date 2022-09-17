332be62a2628f4bb4cac3472dad7addcdfa34f51
Welton Vitor <wvitorsj08@gmail.com>
2022-09-17 16:11:47 -0300

    Atualizando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 sed -i '' -e "s/IMG_PARA_SUBSTITUIR/wviyotsj\/ecommerce-ibm:$TAG/" docker-compose.yml
-docker-compose down -y
-docker-compose up -y
+docker-compose down 
+docker-compose build
+docker-compose up 
