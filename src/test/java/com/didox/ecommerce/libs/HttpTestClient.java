7f3b2d80546a3c015547b26ae21dc2e5f45f6507
Welton Vitor <wvitorsj08@gmail.com>
2022-09-19 14:37:00 -0300

    Atualizando

--- src/test/java/com/didox/ecommerce/libs/HttpTestClient.java
+++ src/test/java/com/didox/ecommerce/libs/HttpTestClient.java
@@ -15,55 +15,55 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import com.google.gson.Gson;
 
 public class HttpTestClient {
-    private static final String host = "http://localhost:8080/";
+    private static final String host = "http://localhost:8090/";
 
-    public static int get(String path) throws ClientProtocolException, IOException{
+    public static int get(String path) throws ClientProtocolException, IOException {
         HttpGet get = new HttpGet(host + path);
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
         CloseableHttpClient client = clientBuilder.build();
         get.addHeader("Content-Type", "application/json");
         var response = client.execute(get);
         return response.getStatusLine().getStatusCode();
     }
 
-    public static int delete(String path) throws ClientProtocolException, IOException{
+    public static int delete(String path) throws ClientProtocolException, IOException {
         HttpDelete get = new HttpDelete(host + path);
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
         CloseableHttpClient client = clientBuilder.build();
         get.addHeader("Content-Type", "application/json");
         var response = client.execute(get);
         return response.getStatusLine().getStatusCode();
     }
 
-    public static int post(String path, Object objeto) throws ClientProtocolException, IOException{
+    public static int post(String path, Object objeto) throws ClientProtocolException, IOException {
         HttpPost post = new HttpPost(host + path);
 
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
         CloseableHttpClient client = clientBuilder.build();
         post.addHeader("Content-Type", "application/json");
 
-		String jsonFile = new Gson().toJson(objeto);
-		HttpEntity entity = new StringEntity(jsonFile);
-		post.setEntity(entity);
+        String jsonFile = new Gson().toJson(objeto);
+        HttpEntity entity = new StringEntity(jsonFile);
+        post.setEntity(entity);
 
         var response = client.execute(post);
-        
+
         return response.getStatusLine().getStatusCode();
     }
 
-    public static int put(String path, Object objeto) throws ClientProtocolException, IOException{
+    public static int put(String path, Object objeto) throws ClientProtocolException, IOException {
         HttpPut put = new HttpPut(host + path);
 
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
         CloseableHttpClient client = clientBuilder.build();
         put.addHeader("Content-Type", "application/json");
 
-		String jsonFile = new Gson().toJson(objeto);
-		HttpEntity entity = new StringEntity(jsonFile);
-		put.setEntity(entity);
+        String jsonFile = new Gson().toJson(objeto);
+        HttpEntity entity = new StringEntity(jsonFile);
+        put.setEntity(entity);
 
         var response = client.execute(put);
-        
+
         return response.getStatusLine().getStatusCode();
     }
 }
